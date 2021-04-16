06c3892340dafe3866b3157dbb87729f1914d0bd
lukasberk <lukasvdberk@gmail.com>
2021-04-16 11:07:48 +0200

    [Fixed] Index routing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,11 @@
 <script>
     import {goto} from "@sapper/app.mjs";
     import {onMount} from "svelte";
+    import LoadingSpinner from "../components/shared/LoadingSpinner.svelte";
 
     onMount(() => {
         goto('/servers', {})
     })
 </script>
+<span>Redirecting to server page</span>
+<LoadingSpinner/>
